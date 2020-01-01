cf6a965ef19deab8e461046c2d6592ab39656d7d
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-01 02:34:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,11 +17,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase()
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "Hi!"
-    "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "WHAT DID YOU EAT TODAY?"
-    "NO, NOT SINCE 1938!"
-  elsif phrase == "WHAT?"
-    "NO, NOT SINCE 1938!"
+  else 
+    
   end 
 end 

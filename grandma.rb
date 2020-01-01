9f4a86b7a8d50749e5e5ea6106b7f77a15b94ccf
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-01 02:34:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase()
+  if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "Hi Nana, how are you?"
+  elsif phrase != phrase.upcase()
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "Hi!"
     "HUH?! SPEAK UP, SONNY!"
